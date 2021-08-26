1980c94934b31ce7c2f9529393778924bef10daf
FelipePavan13 <felipepavang@hotmail.com>
2021-08-26 13:21:43 -0300

    fix colors

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
-import logo from '../../images/logo.png' 
+import logo from '../../images/logo.svg' 
 
 const Container = styled.header`
   width: 100%;
   height: 100px;
-  background-color: #ff0000;
+  background-color: #FF1C1C;
   display: flex;
   align-items: center;
   justify-content: space-around;
