e0288cd0beaf9977adb3112c5453b34a52753aa4
FelipePavan13 <felipepavang@hotmail.com>
2021-08-27 14:41:49 -0300

    added Header and Menu storybooks

---
+++ src/stories/components/Header/Header.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Header from '../../../components/Header/Header';
+
+export default {
+  title: 'Header/Header',
+  component: Header
+}
+
+export const Component = () => <Header />
