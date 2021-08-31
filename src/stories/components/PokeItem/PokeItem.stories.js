22fd2a8ad90b2876884256db406453e0872c74ce
FelipePavan13 <felipepavang@hotmail.com>
2021-08-31 17:03:26 -0300

    added PokeItem storybook

---
+++ src/stories/components/PokeItem/PokeItem.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import PokeItem from '../../../components/PokeItem/PokeItem'
+
+export default {
+  title: 'PokeList/çItem',
+  component: PokeItem
+}
+
+export const Component = () => <PokeItem />
