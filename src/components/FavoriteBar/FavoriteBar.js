9ad4e90398b50dcc3ada89ca44fadae23a0ff83f
FelipePavan13 <felipepavang@hotmail.com>
2021-09-02 14:49:54 -0300

    created FavoriteBar component

---
+++ src/components/FavoriteBar/FavoriteBar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Bar } from './styles/styles'
+
+export default function FavoriteBar() {
+  return (
+    <Bar></Bar>
+  )
+}
---
+++ src/components/FavoriteBar/styles/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Bar = styled.div`
+  width: 70%;
+  height: 100px;
+  background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export { Bar }
