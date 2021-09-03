cb4462b239698917adac1097beb5a7e0f87a7841
Felipe Pavan <felipepavang@hotmail.com>
2021-09-03 16:22:57 -0300

    adding hover to FavoriteItem

--- src/components/FavoriteItem/styles.js
+++ src/components/FavoriteItem/styles.js
@@ -8,6 +8,14 @@ const Card = styled.div`
   border-radius: 5px;
   border: 1px solid black;
   box-shadow: 1px 1px 5px black;
+  transition: all 0.2s;
+  transform: scale(0.9);
+  cursor: pointer;
+
+  :hover {
+    transform: scale(1);
+    box-shadow: 1px 1px 15px black;
+  }
 `
 
 const Image = styled.img`
